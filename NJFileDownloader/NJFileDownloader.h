3c75625cd9165b1201a84226aea6957c7c1b9ac9
fengnianji <765409243@qq.com>
2016-08-01 21:52:45 +0800

    add resumeData support

--- NJFileDownloader/NJFileDownloader.h
+++ NJFileDownloader/NJFileDownloader.h
@@ -10,17 +10,21 @@
 
 @protocol NJFileDownloaderTask <NSObject>
 
+@property (nonatomic, strong) NSDictionary *userInfo;
+
 - (double)fractionCompleted;
 
 - (void)pause;
 - (void)resume;
 - (void)cancel;
+- (void)cancelByProducingResumeData:(void (^)(NSData *resumeData))completionHandler;
 
 @end
 
 @interface NJFileDownloader : NSObject
 
 @property (nonatomic, strong) NSOperationQueue *callbackQueue;  //default is main queue
+@property (nonatomic, assign) BOOL allowsCellularAccess;
 
 - (id<NJFileDownloaderTask>)downloadRequest:(NSURLRequest *)request
                                      toPath:(NSString *)resultPath
@@ -32,5 +36,12 @@
                                    progress:(void (^)(id<NJFileDownloaderTask> downloadTask))downloadProgressHandler
                                  completion:(void(^)(NSError *error))completionHandler;
 
+- (id<NJFileDownloaderTask>)downloadWithResumeData:(NSData *)resumeData
+                                            toPath:(NSString *)resultPath
+                                          progress:(void (^)(id<NJFileDownloaderTask> downloadTask))downloadProgressHandler
+                                        completion:(void(^)(NSError *error))completionHandler;
+
+
++ (NSData *)getResumeData:(NSError *)error;
 
 @end
